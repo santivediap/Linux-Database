587562564766fb6faf369387d01a1083417e7f04
santivediap <santivediap@gmail.com>
2024-02-17 23:35:52 +0100

    Reorder code

--- database.sh
+++ database.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+# Checks if there is not arguments in the command
 if [[ "$#" -eq 0 ]]; then
-  echo "❌️ You did not set arguments! ❔️ Need help? Use bash database.sh help"
+  echo "❌️ You did not set enough arguments! ❔️ Need help? Use bash database.sh help"
+  exit
+fi
+
+# Sends commands guide if help is the first argument
+if [[ "$1" == "help" ]]; then
+  echo "------------------------------------------------ ❔ Commands Guide ❔ ------------------------------------------------"
+  echo "· create table ( table_name ) -> Creates a new table"
+  echo "· insert ( table_name ) ( value_name ) -> Inserts a new value into an existing table"
+  echo "· delete [ table | table_value ] ( value_name ) -> Deletes a table (or a table value) whose name equals value_name"
+  echo "· list-tables -> Lists all existing tables"
+  echo "· list-tables-values ( table_name ) -> Lists all values from an existing table"
+  echo "----------------------------------------------------------------------------------------------------------------------"
+ exit
 else
-  if [[ "$1" == "help" ]]; then
-    echo "------------------------------------------------ ❔️ Commands Guide ❔️ ------------------------------------------------"
-    echo "· create table ( table_name ) -> Creates a new table"
-    echo "· insert ( table_name ) ( value_name ) -> Inserts a new value into an existing table"
-    echo "· delete [ table | table_value ] ( value_name ) -> Deletes a table (or a table value) whose name equals value_name"
-    echo "· list-tables -> Lists all existing tables"
-    echo "· list-tables-values ( table_name ) -> Lists all values from an existing table"
-    echo "----------------------------------------------------------------------------------------------------------------------"
+  # If first argument is not help and given arguments are less than 3, send error
+  if [[ "$#" -lt 3 ]]; then
+    echo "❌ You did not set enough arguments! ❔ Need help? Use bash database.sh help"
+    exit
   fi
 fi
